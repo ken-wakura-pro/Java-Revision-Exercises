f88e1edbde08eece1a8ff08e11e4f12f3edac3f0
wakura-mbuya <wakura9@gmail.com>
2021-08-17 22:04:27 +0300

    This folder consists of solution to exercise in "Introduction to Java programming by David J. Eck  (eck@hws.edu)"

---
+++ diceRollExperiment/src/dicerollexperiment/DiceRollExperiment.java
@@ -0,0 +1,24 @@
+/*
+* Exercise 3.1:
+* 
+*/
+package dicerollexperiment;
+
+/**
+ * How many times do you have to roll a pair of dice before they come 
+ * up snake eyes? You could do the experiment by rolling the dice by hand.
+ * Write a computer program that simulates the experiment. The program should 
+ * report the number of rolls that it makes before the dice come up snake eyes.
+ * (Note: "Snake eyes" means that both dice show a value of 1.
+ * @author wakura
+ */
+public class DiceRollExperiment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
